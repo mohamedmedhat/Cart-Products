e8534b0202d9fff0ece5508bd31f0f9c33049da1
moMedhat <mmr973320@gmail.com>
2024-09-17 15:26:47 +0300

    [add]: add dependencies and  database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.getOrThrow<string>('RDS_HOST'),
+        port: config.getOrThrow<number>('RDS_PORT'),
+        username: config.getOrThrow<string>('RDS_USER_NAME'),
+        password: config.getOrThrow<string>('RDS_PASSWORD'),
+        database: config.getOrThrow<string>('RDS_DATABASE'),
+        entities: [],
+        autoLoadEntities: true,
+        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+        seeds: [__dirname + '/seeds/**/*{.ts,.js}'],
+        factories: [__dirname + '/factories/**/*{.ts,.js}'],
+        cli: {
+          migrationsDir: __dirname + '/migrations/',
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
